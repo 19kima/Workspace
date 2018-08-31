362c0020ba73436dcd205aaca81f75be414712c9
Andrew Kim <19kima@elmbrookstudents.org>
2018-08-31 11:51:49 -0500

    stuff

--- DataStructures/src/project1/Letter.java
+++ DataStructures/src/project1/Letter.java
@@ -1,3 +1,10 @@
+/**
+ * COSC 2100 - Project 1
+ * Explain briefly the functionality of the class.
+ * @author Andrew Kim
+ * Instructor Brylow
+ * TA-BOT:MAILTO andrew.y.kim@marquette.edu
+ */
 package project1;
 import java.util.*;
 import java.io.*;
@@ -9,56 +16,50 @@ public class Letter {
 		Scanner input = new Scanner(temp);
 		int row = 0;
 		int col = 0;
-		int stop1 = 0;
-		int stop2 = 0;
+		int stop = 0;
 		String message = "";
-		do {
-			String rowCounter = input.nextLine();
-			if(rowCounter.equals("---")) {
-				stop1 = 1;
-			} else {
-				row++;
+		if (input.hasNextLine()) {						//Check for empty file
+			do {
+				String rowCounter = input.nextLine();	
+				if(rowCounter.equals("---")) {
+					stop = 1;
+				} else {
+					row++;								//row counter
+				}
+			}while(stop == 0);
+			while (input.hasNextLine()) {				//Record mail message
+				message+=input.nextLine() + "\n";
 			}
-		}while(stop1 == 0);
-		do {
-			String rowCounter = input.nextLine();
-			if(rowCounter.equals("---")) {
-				message+=rowCounter;
-				stop2 = 1;
-			} else {
-				message+=rowCounter + "\n";
-			}
-		}while(stop2 == 0);
-		input.close();
-		input = new Scanner(temp);
-		String[] tags = input.nextLine().split(" ");
-		col = tags.length;
-		boolean allGood = true;
-		for (int i = 1; i < row; i++) {
-			if (input.nextLine().split(" ").length != col) {
-				allGood = false;
-			}
-		}
-		input.close();
-		if (allGood == true) {
+			message+= "---";
+			input.close();
 			input = new Scanner(temp);
-			String[][] data = new String[row][col];
-			for (int j = 0; j < row; j++) {
-				for(int k = 0; k < col; k++) {
-					data[j][k] = input.next();
+			col = input.nextLine().split(" ").length;	//col counter
+			boolean allGood = true;
+			for (int i = 1; i < row; i++) {
+				if (input.nextLine().split(" ").length != col) {		//check if all the rows have same amount of col
+					allGood = false;
 				}
 			}
 			input.close();
-			String changer = message.substring(0);
-			for(int j = 1; j < row; j++) {
-				for (int k = 0; k < col; k++) {
-					changer = changer.replaceAll(data[0][k], data[j][k]);
+			if (allGood == true) {
+				input = new Scanner(temp);
+				String[][] data = new String[row][col];				//fill arrays
+				for (int j = 0; j < row; j++) {
+					for(int k = 0; k < col; k++) {
+						data[j][k] = input.next();
+					}
 				}
-				System.out.println(changer);
-				changer=message.substring(0);
+				input.close();
+				String changer = new String(message);
+				for(int j = 1; j < row; j++) {
+					for (int k = 0; k < col; k++) {
+						changer = changer.replaceAll(data[0][k], data[j][k]);	//change message
+					}
+					System.out.println(changer);
+					changer=new String(message);
+				}
 			}
-		} else {
-			System.out.println("bro something wrong with your shit");
 		}
 	}
+
 }
